facda2a706250fff17f690db82f5a2b7ce1697e4
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-03-22 14:09:57 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
 function App() {
   return (
     <>
-      <h1>My app</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   );
 }
 
 export default App;
-
-// 80. 3:30 Rules
